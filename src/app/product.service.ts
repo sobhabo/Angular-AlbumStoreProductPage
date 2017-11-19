816a34e5b3369c6746012272d9e528e42a3b84f1
sobhabo <sobhabo@gmail.com>
2017-11-19 17:45:16 -0600

    prp23456
    jjjljkjnsksnc f

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http,Response} from '@angular/http';
 @Injectable()
 export class ProductService {
- private albumUrl='../assets/album.json';
+ private _albumUrl='../assets/album.json';
   constructor(private _http: Http) {
 
   }
