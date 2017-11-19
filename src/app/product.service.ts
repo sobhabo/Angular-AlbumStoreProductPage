0f0ecb2ecfc49b49294f8e94577dbff71a53409a
sobhabo <sobhabo@gmail.com>
2017-11-19 17:36:10 -0600

    property
    added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import {Http,Response} from '@angular/http';
 @Injectable()
 export class ProductService {
-albumUrl='../assets/album.json';
+albumUrl :'../assets/album.json';
   constructor(private _http: Http) {
 
   }
   getAlbum(id:number){
+
+
 
 
   }
