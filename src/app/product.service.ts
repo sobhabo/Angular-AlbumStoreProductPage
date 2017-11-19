1a05b5d0c92ad4fcde4ca265ecfed1fe45a2f525
sobhabo <sobhabo@gmail.com>
2017-11-19 17:58:29 -0600

    nsn

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http,Response} from '@angular/http';
-import rxjs/add/operator/map from '@angular/core';
+import {rxjs/add/operator/map} from '@angular/core';
 @Injectable()
 export class ProductService {
  private _albumUrl='../assets/album.json';
